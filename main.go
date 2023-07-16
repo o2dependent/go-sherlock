edbb79383ee5cd09aaacaf4614d60a7f795dc3ab
Ethan Olsen <131eolsen@gmail.com>
2023-07-16 15:12:15 -0700

    chore: update test command for visibility

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
-package gosherlock
+package sherlock
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 func Test() {
-	cmd := exec.Command("ls", "-l")
-	cmd.Run()
+	cmd := exec.Command("ls")
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
 }
